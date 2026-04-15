c93cedd4f276793fcf10cd7d0d140aa18c50ed03
Francesco Cassese <francescocassese9@gmail.com>
2026-04-15 16:20:19 +0200

    refactror: rimosso il for dal main

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,3 @@
 
 const lista = document.querySelector('#email-list');
 const btnRigenera = document.querySelector('#btn-refresh');
-
-//CICLO
-for (let i = 0; i < 10; i++) {                                       //Faccio partire il ciclo per 10 interazioni
-    fetch('https://flynn.boolean.careers/exercises/api/random/mail') //Ad ogni interazione fa una richiesta per avere un email random
-        .then(risposta => {                                          //Quando il server mi risponde 
-            return risposta.json();                                  //Trasformo i dati in json
-        })
-        .then(dati => {                                              //dopo la trasformazione leggo il contenuto
-            stampaElemLista(dati.response);
-        })
-}
