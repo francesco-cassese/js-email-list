add2ce6b2317289a82e2f8a1a5e5c6905b97703a
Francesco Cassese <francescocassese9@gmail.com>
2026-04-15 16:39:31 +0200

    feat: richiamata funzione per stampare la lista fin da subito, e richiamata la stessa funzione nel bottone

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,7 @@
 
 const lista = document.querySelector('#email-list');
 const btnRigenera = document.querySelector('#btn-refresh');
+
+rigeneraLista();                                                //Richiamo funzione per poter stampare la lista all'inizio
+
+btnRigenera.addEventListener('click', rigeneraLista);           //Collego la funzione al bottone per poter fare il refresh alla pagine
