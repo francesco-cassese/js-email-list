120a709dcb4c554e8f6f982c890edbdeb5f371e6
Francesco Cassese <francescocassese9@gmail.com>
2026-04-15 17:10:11 +0200

    refactor: creato un array vuoto e modificata logica funzione per poter stampare tutta la lista allo stesso momento

--- scripts/function.js
+++ scripts/function.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// FUNZIONE CHIAMATA SERVER
+// --- FUNZIONE CHIAMATA SERVER ---
 const chiamataApi = () => {
     return fetch('https://flynn.boolean.careers/exercises/api/random/mail')         //Mando la richiesta al server
         .then(risposta => {                                                         //Quando il server mi risponde 
@@ -11,20 +11,31 @@ const chiamataApi = () => {
         })
 }
 
-// FUNZIONE STAMPA LISTA
+// --- FUNZIONE STAMPA LISTA ---
 
-const stampaElemLista = email => {
-    lista.innerHTML += `<li>${email}</li>`;                                         //Creo la riga della lista
+const stampaElemListaIntera = arrayEmail => {
+    let contenutoHtml = '';                                                         //Stringa d'appogio
+    for (let i = 0; i < arrayEmail.length; i++) {
+        contenutoHtml += `<li>${arrayEmail[i]}</li>`;                               //Creo la riga della lista 
+    }
+    lista.innerHTML = contenutoHtml;                                                //sovrascrivo il contenuto   
 }
 
-// FUNZIONE RIGENERA LISTA
+// --- FUNZIONE RIGENERA LISTA ---
+
+const listaEmail = [];                                                              //Creo un array vuoto per accumulare le 10 email  
 
 const rigeneraLista = () => {
-    lista.innerHTML = "";                                                           //Pulisco la lista vechia
+    lista.innerHTML = "";                                                           //Pulisco la lista precedente
+    listaEmail.length = 0;                                                          //La faccio ripartire da 0
 
     for (let i = 0; i < 10; i++) {                                                  //inizio un ciclo con 10 interazioni
-        chiamataApi().then(email =>                                                 //Ad ogni interazione faccio una richiesta al server
-            stampaElemLista(email)                                                  //appena mi rispode stampo la nuova lista in pagina
-        );
+        chiamataApi().then(email => {                                               //Ad ogni interazione faccio una richiesta al server
+            listaEmail.push(email);                                                 //appena mi rispode la pusho nel array
+
+            if (listaEmail.length === 10) {                                         //quando l'array raggiunge i 10 elementi
+                stampaElemListaIntera(listaEmail);                                  // stampo l'intera lista
+            }
+        });
     }
 }
