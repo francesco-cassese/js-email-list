661240c50dfa4fbccd2444b14b40ebb45bd89936
Francesco Cassese <francescocassese9@gmail.com>
2026-04-15 15:50:53 +0200

    feat: aggiunti collegamenti agli elementi, costante d'appoggio e logica ciclo

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// COLLEGAMENTI
+
+const lista = document.querySelector('email-list');
+const btnRigenera = document.querySelector('btn-refresh');
+
+//COSTANTE D'APPOGGIO
+const listaEmail = [];                                               // Creo un array vuoto per accumulare gli indirizzi
+
+//CICLO
+for (let i = 0; i < 10; i++) {                                       //Faccio partire il ciclo per 10 interazioni
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail') //Ad ogni interazione fa una richiesta per avere un email random
+        .then(risposta => {                                          //Quando il server mi risponde 
+            return risposta.json();                                  //Trasformo i dati in json
+        })
+        .then(dati => {                                              //dopo la trasformazione leggo il contenuto
+            listaEmail.push(dati.response);                          //pusho nell'array solo l'email random
+        })
+}
