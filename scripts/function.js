3072f202beb4d218cd3c4f92b2d7e33f4fb57121
Francesco Cassese <francescocassese9@gmail.com>
2026-04-15 16:20:02 +0200

    refactor: aggiunta chiamata api in una funzione per poterla collegare al bottone successivamente

--- scripts/function.js
+++ scripts/function.js
@@ -1,7 +1,18 @@
 'use strict';
 
+// FUNZIONE CHIAMATA SERVER
+const chiamataApi = () => {
+    return fetch('https://flynn.boolean.careers/exercises/api/random/mail')         //Mando la richiesta al server
+        .then(risposta => {                                                         //Quando il server mi risponde 
+            return risposta.json();                                                 //Trasformo i dati in json
+        })
+        .then(dati => {                                                             //dopo la trasformazione leggo il contenuto
+            return dati.response;                                                   //Restituisco solo l'email 
+        })
+}
+
 // FUNZIONE STAMPA LISTA
 
-const stampaElemLista = (email) => {
+const stampaElemLista = email => {
     lista.innerHTML += `<li>${email}</li>`;
 }
